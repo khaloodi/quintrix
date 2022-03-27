cdf0c13177c8aec357b3706f782f0e37d410b01b
Khaled Adad <khaledadad@lewisu.edu>
2022-03-27 16:08:13 -0500

    added comments to SleepDbDao.kt and started building queries

--- Android Studio/andfun-kotlin-sleep-tracker-starter-code/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
+++ Android Studio/andfun-kotlin-sleep-tracker-starter-code/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
@@ -16,4 +16,23 @@
 
 package com.example.android.trackmysleepquality.database
 
-interface SleepDatabaseDao
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Update
+
+@Dao // all Dao's need to be annotated with the Dao keyword
+interface SleepDatabaseDao {
+    // * add an annotated insert() method for inserting a single SleepNight
+    @Insert // we need two things, an insert annotation and a function
+    // definition for an insert function that we can call from our code
+    fun insert(night: SleepNight) // insert function definition
+    // during compilation, room will generate code to turn this passed in
+    // Kotlin object "night" into a row of values for the database;
+    // room creates the row from the entity object and inserts it into the DB
+
+    // * add an annotated upddate() method for updating a SleepNight
+    @Update
+    fun update(night: SleepNight) // notice Insert and Update are simple
+    // for the rest we'll have to write SQL queries to perform the specific operations
+}
+
