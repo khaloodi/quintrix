b44c95537ac013586ca70bdea065b318b3d26f5a
Khaled Adad <khaledadad@lewisu.edu>
2022-03-28 19:17:25 -0500

    wip

---
+++ Android Studio/PhotoGallery/app/src/main/java/com/example/photogallery/api/FlickerFetcher.kt
@@ -0,0 +1,48 @@
+package com.example.photogallery.api
+
+import android.content.ContentValues.TAG
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.photogallery.api.FlickerApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
+class FlickrFetcher {
+    private val flickerApi: FlickerApi
+    init {
+
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl("https://www.flickr.com")
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .build()
+        flickerApi = retrofit.create(FlickerApi::class.java)
+    }
+    fun fetchContents(): LiveData<String> {
+        val responseLiveData: MutableLiveData<String> = MutableLiveData()
+        val flickrRequest: Call<String> = flickerApi.fetchContents()
+        flickrRequest.enqueue(object : Callback<String> {
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                Log.e(TAG, "Failed to fetch photos", t)
+            }
+            override fun onResponse(
+                call: Call<String>,
+                response: Response<String>
+            ) {
+                Log.d(TAG, "Response received")
+                responseLiveData.value = response.body()
+            }
+        })
+        return responseLiveData
+    }
+}
