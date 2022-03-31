af88f9cc1ea93ce5d184bf7a5254bdc73de03345
Khaled Adad <khaledadad@lewisu.edu>
2022-03-30 19:03:20 -0500

    navigation working b/w initial fragments, nav and game

--- Android Studio/andfun-kotlin-android-trivia-starter-code/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ Android Studio/andfun-kotlin-android-trivia-starter-code/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 /**
@@ -28,8 +29,19 @@ class TitleFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(
-            inflater, R.layout.fragment_title, container, false
+            inflater, R.layout.fragment_title, container, false)
+
+        // ************
+//        binding.playButton.setOnClickListener { view: View -> // here we are passing an anonymous function into listener
+//            // Navigation.findNavController(view).navigate((R.id.action_titleFragment_to_gameFragment))
+//            view.findNavController(view).navigate((R.id.action_titleFragment_to_gameFragment))
+//        }
+        // ************
+        // the code above
+        binding.playButton.setOnClickListener (
+            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
         )
+
 //        return inflater.inflate(R.layout.placeholder_layout, container, false)
         return binding.root
     }
