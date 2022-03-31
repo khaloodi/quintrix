e1b8d89d3aa17e8fae5170ee2c9dfb731b4e6277
Khaled Adad <khaledadad@lewisu.edu>
2022-03-30 23:14:18 -0500

    about menu working in activity bard, had to add data to xml menu item

--- Android Studio/andfun-kotlin-android-trivia-starter-code/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ Android Studio/andfun-kotlin-android-trivia-starter-code/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -1,12 +1,17 @@
 package com.example.android.navigation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 /**
@@ -14,54 +19,76 @@ import com.example.android.navigation.databinding.FragmentTitleBinding
  * Use the [TitleFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
+
 class TitleFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-
-        }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_title, container, false)
-
-        // ************
-//        binding.playButton.setOnClickListener { view: View -> // here we are passing an anonymous function into listener
-//            // Navigation.findNavController(view).navigate((R.id.action_titleFragment_to_gameFragment))
-//            view.findNavController(view).navigate((R.id.action_titleFragment_to_gameFragment))
-//        }
-        // ************
-        // the code above
         binding.playButton.setOnClickListener (
-            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
-        )
-
-//        return inflater.inflate(R.layout.placeholder_layout, container, false)
+            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
+        setHasOptionsMenu(true)
         return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TitleFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TitleFragment().apply {
-                arguments = Bundle().apply {
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overflow_menu, menu)
+    }
 
-                }
-            }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController())
+                || super.onOptionsItemSelected(item)
     }
 }
+
+
+
+//class TitleFragment : Fragment() {
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        arguments?.let {
+//
+//        }
+//    }
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        // Inflate the layout for this fragment
+//        val binding: FragmentTitleBinding = DataBindingUtil.inflate(
+//            inflater, R.layout.fragment_title, container, false)
+//
+//        // ************
+////        binding.playButton.setOnClickListener { view: View -> // here we are passing an anonymous function into listener
+////            // Navigation.findNavController(view).navigate((R.id.action_titleFragment_to_gameFragment))
+////            view.findNavController(view).navigate((R.id.action_titleFragment_to_gameFragment))
+////        }
+//        // ************
+//        // the code above
+//        binding.playButton.setOnClickListener (
+//            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+//
+//
+//
+//
+//        )
+//        // tell Android that we are going to have a menu item associated w/our TitleFragment
+//        setHasOptionsMenu(true)
+////        return inflater.inflate(R.layout.placeholder_layout, container, false)
+//        return binding.root
+//    }
+//
+//    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+//        super.onCreateOptionsMenu(menu, inflater)
+//        inflater?.inflate(R.menu.overflow_menu, menu) // inflate menu using one-liner
+//    }
+//
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean { // when a menu item is selected, this method
+//        // that we are overriding is called
+//        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController())
+//                || super.onOptionsItemSelected(item)
+//    }
+//}
