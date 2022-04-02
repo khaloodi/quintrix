0bed506f88e4ff03de210082a0f80da95f8845b2
Khaled Adad <khaledadad@lewisu.edu>
2022-04-02 18:59:02 -0500

    add starter code

--- Kotlin Progamming Problems & Solutions/list_and_loop_practice/src/main/kotlin/Main.kt
+++ Kotlin Progamming Problems & Solutions/list_and_loop_practice/src/main/kotlin/Main.kt
@@ -1,3 +1,12 @@
-fun main(args: Array<String>) {
-    
+open class Item(val name: String, val price: Int)
+
+class Noodles : Item("Noodles", 10)
+
+class Vegetables : Item("Vegetables", 5)
+
+fun main() {
+    val noodles = Noodles()
+    val vegetables = Vegetables()
+    println(noodles)
+    println(vegetables)
 }
