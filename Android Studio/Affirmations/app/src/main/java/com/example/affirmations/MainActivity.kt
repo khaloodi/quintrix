36da09e84d2f3d3a7369c73563e95055dfd4214e
Khaled Adad <khaledadad@lewisu.edu>
2022-04-02 21:09:47 -0500

    display number of affirmations in text view

--- Android Studio/Affirmations/app/src/main/java/com/example/affirmations/MainActivity.kt
+++ Android Studio/Affirmations/app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.affirmations.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView: TextView = findViewById(R.id.textview)
+
+        // todo Add code to create and display the size of the affirmations list.
+        //  Create a Datasource, call loadAffirmations(), get the size of the returned list,
+        //  convert it to a string, and assign it as the text of textView
+        textView.text = Datasource().loadAffirmations().size.toString()
+
     }
 }
---
+++ Android Studio/Affirmations/app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -0,0 +1,4 @@
+package com.example.affirmations.model
+
+// add data keyword to show this is a data class
+data class Affirmation(val stringResourceId: Int)// Add a val integer parameter stringResourceId to the constructor of the Affirmation class
