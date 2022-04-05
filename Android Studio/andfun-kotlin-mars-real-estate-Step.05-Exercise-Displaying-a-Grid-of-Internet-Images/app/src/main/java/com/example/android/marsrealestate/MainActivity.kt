f354076184c7514553b18c9116642031761e3beb
Khaled Adad <khaledadad@lewisu.edu>
2022-04-05 14:43:40 -0500

    added real-estate project from step 5 because I updated project and it caused several internal errors

--- Android Studio/andfun-kotlin-mars-real-estate-Step.05-Exercise-Displaying-a-Grid-of-Internet-Images/app/src/main/java/com/example/android/marsrealestate/MainActivity.kt
+++ Android Studio/andfun-kotlin-mars-real-estate-Step.05-Exercise-Displaying-a-Grid-of-Internet-Images/app/src/main/java/com/example/android/marsrealestate/MainActivity.kt
@@ -15,18 +15,19 @@
  *
  */
 
-package com.example.android.marsrealestate.network
+package com.example.android.marsrealestate
 
-import com.squareup.moshi.Json
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-/**
- * This data class defines a Mars property which includes an ID, the image URL, the type (sale
- * or rental) and the price (monthly if it's a rental).
- * The property names of this data class are used by Moshi to match the names of values in JSON.
- */
-data class MarsProperty(
-        val id: String,
-        // used to map img_src from the JSON to imgSrcUrl in our class
-        @Json(name = "img_src") val imgSrcUrl: String,
-        val type: String,
-        val price: Double)
+class MainActivity : AppCompatActivity() {
+
+    /**
+     * Our MainActivity is only responsible for setting the content view that contains the
+     * Navigation Host.
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
