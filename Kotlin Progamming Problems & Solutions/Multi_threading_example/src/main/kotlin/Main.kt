bd79fb511778ce8f8e85e3de581ba83ce18b1189
Khaled Adad <khaledadad@lewisu.edu>
2022-04-06 14:13:18 -0500

    three things running at once, parallel processing

--- Kotlin Progamming Problems & Solutions/Multi_threading_example/src/main/kotlin/Main.kt
+++ Kotlin Progamming Problems & Solutions/Multi_threading_example/src/main/kotlin/Main.kt
@@ -1,3 +1,38 @@
 fun main(args: Array<String>) {
-    
+    var t1= thread("thread1")
+    t1.start() //in order to make thread class operate as a thread, have to call .start()
+
+    var t2 = thread("thread2")
+    t2.start()
+
+    var t3 = thread("thread3")
+    t3.start()
+
+    println(" thread is run") // notice this line is run, as well as Thread code ... non-blocking
+}
+
+class thread: Thread {
+    var ThreadName:String? = null
+
+    constructor(ThreadName:String):super() {
+        this.ThreadName = ThreadName
+        println("${this.ThreadName} has started")
+    }
+
+    override fun run() {
+        // Write Thread
+
+        var count = 0
+
+        while(count<10) {
+            println("ThreadName: ${this.ThreadName} Count: $count")
+            count++
+
+            try {
+                Thread.sleep(1000)
+            } catch (ex: Exception) {
+                println(ex.message)
+            }
+        }
+    }
 }
