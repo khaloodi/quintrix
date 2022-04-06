4282aaaf86b5c5d4d9f22ebb257e7feac0422ce3
Khaled Adad <khaledadad@lewisu.edu>
2022-04-06 12:12:45 -0500

    wip

--- Kotlin Progamming Problems & Solutions/Comparable_Sort_array_of_objects/src/main/kotlin/Main.kt
+++ Kotlin Progamming Problems & Solutions/Comparable_Sort_array_of_objects/src/main/kotlin/Main.kt
@@ -1,3 +1,22 @@
+import java.util.*
+import kotlin.collections.ArrayList
+
 fun main(args: Array<String>) {
-   
+    var listOfNames = ArrayList<String>()
+
+    listOfNames.add("Walter")
+    listOfNames.add("Nada")
+    listOfNames.add("Zena")
+    listOfNames.add("Khaled")
+
+    println("Names before sorting: ")
+    for (name in listOfNames) {
+        println("Name: " + name)
+    }
+
+    Collections.sort(listOfNames)
+    println("Names after sorting: ")
+    for (name in listOfNames) {
+        println("Name: " + name)
+    }
 }
