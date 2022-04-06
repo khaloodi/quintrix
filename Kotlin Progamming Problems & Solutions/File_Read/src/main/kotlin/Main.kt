6ee3eb2a84cf94317dcaa7126a28f16b8f6fc474
Khaled Adad <khaledadad@lewisu.edu>
2022-04-06 12:59:59 -0500

    writing or reading to a file based on user input

--- Kotlin Progamming Problems & Solutions/File_Read/src/main/kotlin/Main.kt
+++ Kotlin Progamming Problems & Solutions/File_Read/src/main/kotlin/Main.kt
@@ -1,10 +1,24 @@
+import java.io.FileReader
 import java.io.FileWriter
 import java.io.Writer
 
 fun main(args: Array<String>) {
-    print("Write to text file: ")
-    var str:String = readLine().toString()
-    WriteToFile(str)
+    println("1- read \n2- write \n")
+    var op = readLine()!!.toInt()
+
+    when(op) {
+        1 -> ReadFromFile() // ReadFromFile()
+        2 -> {
+            print("Write to text file: ")
+            var str:String = readLine().toString()
+            WriteToFile(str)
+        }
+    }
+
+
+    // print("Write to text file: ")
+    // var str:String = readLine().toString()
+    // WriteToFile(str)
 }
 
 fun WriteToFile(str: String) {
@@ -15,4 +29,22 @@ fun WriteToFile(str: String) {
     } catch (ex: Exception) {
         println(ex.message)
     }
+}
+
+fun ReadFromFile() {
+    try {
+        var fin = FileReader("test.txt")
+        var c:Int? // use to read the character from the file
+
+        do {
+            c=fin.read() // take ASCII code of first character and read
+            print(c.toChar())
+
+        } while (c != -1)
+
+    } catch (ex: Exception) {
+        print(ex.message) // if cannot read the file
+    }
+
+
 }
