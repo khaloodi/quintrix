3929c41c03a50131d4ca3fdd9960972b81efefa1
Khaled Adad <khaledadad@lewisu.edu>
2022-04-07 22:24:47 -0500

    added layout code for dogglers

--- Android Studio/android-basics-kotlin-dogglers-app-main/app/src/main/java/com/example/dogglers/adapter/DogCardAdapter.kt
+++ Android Studio/android-basics-kotlin-dogglers-app-main/app/src/main/java/com/example/dogglers/adapter/DogCardAdapter.kt
@@ -19,6 +19,7 @@ import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.dogglers.model.Dog
 
 /**
  * Adapter to inflate the appropriate list item layout and populate the view with information
