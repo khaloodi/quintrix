df11389d5815e42380f4f4953af268e2386c4da4
Khaled Adad <khaledadad@lewisu.edu>
2022-04-24 11:47:44 -0500

    wip

--- Android Studio/andfun-kotlin-dev-bytes-starter-code/app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
+++ Android Studio/andfun-kotlin-dev-bytes-starter-code/app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
@@ -16,3 +16,34 @@
  */
 
 package com.example.android.devbyteviewer.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import com.example.android.devbyteviewer.database.VideosDatabase
+import com.example.android.devbyteviewer.database.asDomainModel
+import com.example.android.devbyteviewer.domain.Video
+import com.example.android.devbyteviewer.network.Network
+import com.example.android.devbyteviewer.network.asDatabaseModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+/**
+ * Repository for fetching devbyte videos from the network and storing them on disk.
+ */
+// responsible for providing a simple api to our data sources
+class VideosRepository(private val database: VideosDatabase) {
+
+    /**
+     * A playlist of videos that can be shown on the screen
+     */
+    val videos: LiveData<List<Video>> = Transformations.map(database.videoDao.getVideos()) {// from the DB
+        it.asDomainModel() // convert from a database video to a video
+    }
+
+    suspend fun refreshVideos() {
+        withContext(Dispatchers.IO) {
+            val playlist = Network.devbytes.getPlaylist().await() // coroutine suspends until this is available, therefore non-blocking
+            database.videoDao.insertAll(*playlist.asDatabaseModel()) // asDataBaseModel maps network results to database objects
+        }
+    }
+}
